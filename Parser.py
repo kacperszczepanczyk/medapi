43d930b054fa7cbd7f258fb69667589e84a92c77
Kacper Szczepańczyk <kacper.szczepanczyk@gmail.com>
2018-06-25 20:49:18 +0200

    init

--- Parser.py
+++ Parser.py
@@ -35,7 +35,6 @@ class Parser:
         for title, _list in zip(titles, lists):
             times = _list.find_all('div', class_='med-width-25')
             activities = _list.find_all('div', class_='med-width-75')
-            msg = ''
             activity = []
             for time, activity in zip(times, activities):
                 msg = time.get_text() + ' ' + activity.get_text()
